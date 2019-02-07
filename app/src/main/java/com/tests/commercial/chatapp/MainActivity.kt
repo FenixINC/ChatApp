0f6d763a80b8a7ce24d7f5cb67d0e1d0225ea1b7
Taras Koloshmatin <taras@pier2marketing.com>
2019-02-07 10:17:25 +0200

    logout

--- app/src/main/java/com/tests/commercial/chatapp/MainActivity.kt
+++ app/src/main/java/com/tests/commercial/chatapp/MainActivity.kt
@@ -3,6 +3,9 @@ package com.tests.commercial.chatapp
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +16,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         mAuth = FirebaseAuth.getInstance()
         val user = mAuth.currentUser
 
@@ -20,5 +26,24 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this@MainActivity, StartActivity::class.java))
             finish()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        this@MainActivity.menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+//            R.id.action_settings -> {}
+            R.id.action_logout -> {
+                FirebaseAuth.getInstance().signOut()
+                startActivity(Intent(this@MainActivity, StartActivity::class.java))
+                finish()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
